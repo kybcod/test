9c230f960b3e2e12d48526decd310178a7408eea
kyb <kyb-0215@naver.com>
2024-10-04 17:52:41 +0900

    lombok 설치 후 @RequiredArgsConstructor를 사용하여 자동 생성자 주입

--- src/main/java/com/example/demo/Member/MemberController.java
+++ src/main/java/com/example/demo/Member/MemberController.java
@@ -1,17 +1,20 @@
 package com.example.demo.Member;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class MemberController {
 	
-	@Autowired
-	private MemberService memberService;
+	private final MemberService memberService;
 	
 	@GetMapping("/join")
 	public String join() {
@@ -19,7 +22,7 @@ public class MemberController {
 	}
 	
 	@PostMapping("/join")
-	public void joinMember(Member member) {
+	public void joinMember(Member member, @RequestParam("profile") MultipartFile profile) {
 		memberService.insert(member);
 	}
 }
--- src/main/java/com/example/demo/Member/MemberService.java
+++ src/main/java/com/example/demo/Member/MemberService.java
@@ -4,13 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import lombok.RequiredArgsConstructor;
+
 
 @Service
 @Transactional(rollbackFor = Exception.class)
+@RequiredArgsConstructor
 public class MemberService {
 	
-	@Autowired
-	private MemberMapper memberMapper;
+	private final MemberMapper memberMapper;
 
 	public void insert(Member member) {
 		memberMapper.insert(member);
